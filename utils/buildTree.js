0f63f6f14d0f718066c6a549d0e483bcaee209a6
ethyla <36268899+ethyla@users.noreply.github.com>
2025-01-06 14:06:49 +0100

    feat: add js proof retrieval example

---
+++ utils/buildTree.js
@@ -0,0 +1,10 @@
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import fs from "fs";
+
+const values = JSON.parse(fs.readFileSync("exampleDistribution.json"))
+
+const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
+
+console.log('Merkle Root:', tree.root);
+
+fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
