49ea9f95e14fd6ee3436c40b3c5caa01b035e7d8
ethyla <36268899+ethyla@users.noreply.github.com>
2025-05-05 18:17:04 +0200

    feat: lock tests

--- test/Unlock.t.sol
+++ test/Unlock.t.sol
@@ -9,11 +9,14 @@ contract UnlockTest is Test, DeployScript {
     KatToken token;
     address alice = makeAddr("alice");
     address beatrice = makeAddr("beatrice");
-
-    address dummyToken = makeAddr("dummyToken");
+    bytes32 UNLOCKER;
+    bytes32 LOCK_EXEMPTION_ADMIN;
 
     function setUp() public {
         token = deployDummyToken();
+
+        UNLOCKER = token.UNLOCKER();
+        LOCK_EXEMPTION_ADMIN = token.LOCK_EXEMPTION_ADMIN();
     }
 
     function test_locked() public {
@@ -28,5 +31,96 @@ contract UnlockTest is Test, DeployScript {
     function test_unlock_early_no() public {
         vm.expectRevert("Not role holder.");
         token.unlockAndRenounceUnlocker();
+    }
+
+    function test_unlock_early() public {
+        vm.prank(dummyDistributor);
+        token.mint(alice, 100);
+
+        vm.prank(alice);
+        vm.expectRevert("Token locked.");
+        token.transfer(beatrice, 10);
+
+        assertEq(token.isUnlocked(), false);
+        vm.prank(dummyUnlocker);
+        token.unlockAndRenounceUnlocker();
+        assertEq(token.isUnlocked(), true);
+        assertEq(token.roleHolder(UNLOCKER), address(0));
+
+        vm.prank(alice);
+        token.transfer(beatrice, 10);
+    }
+
+    function test_unlock_time_based() public {
+        vm.prank(dummyDistributor);
+        token.mint(alice, 100);
+
+        vm.prank(alice);
+        vm.expectRevert("Token locked.");
+        token.transfer(beatrice, 10);
+
+        assertEq(token.isUnlocked(), false);
+        vm.warp(dummyUnlockTime);
+        assertEq(token.isUnlocked(), false);
+        
+        vm.warp(dummyUnlockTime + 1);
+        assertEq(token.isUnlocked(), true);
+        assertEq(token.roleHolder(UNLOCKER), dummyUnlocker);
+
+        vm.prank(alice);
+        token.transfer(beatrice, 10);
+
+        vm.prank(dummyUnlocker);
+        token.unlockAndRenounceUnlocker();
+        assertEq(token.roleHolder(UNLOCKER), address(0));
+    }
+
+    function test_locked_transfer_bypass() public {
+        vm.prank(dummyDistributor);
+        token.mint(dummyDistributor, 10);
+
+        vm.prank(dummyDistributor);
+        token.transfer(beatrice, 10);
+    }
+
+    function test_locked_transfer_bypass_admin() public {
+        vm.prank(dummyDistributor);
+        token.mint(alice, 100);
+
+        vm.prank(alice);
+        vm.expectRevert("Token locked.");
+        token.transfer(beatrice, 10);
+
+        vm.prank(dummyLockExemptionAdmin);
+        token.setWhitelist(alice);
+
+        vm.prank(alice);
+        token.transfer(beatrice, 10);
+
+        vm.prank(beatrice);
+        vm.expectRevert("Token locked.");
+        token.transfer(alice, 10);
+    }
+
+    function test_locked_transfer_bypass_admin_unset() public {
+        vm.prank(dummyDistributor);
+        token.mint(alice, 100);
+
+        vm.prank(alice);
+        vm.expectRevert("Token locked.");
+        token.transfer(beatrice, 10);
+
+        vm.prank(dummyLockExemptionAdmin);
+        token.setWhitelist(alice);
+
+        vm.prank(alice);
+        token.transfer(beatrice, 10);
+
+        vm.prank(dummyLockExemptionAdmin);
+        token.setWhitelist(alice);
+
+        vm.prank(alice);
+        vm.expectRevert("Token locked.");
+        token.transfer(beatrice, 10);
     }
 }
